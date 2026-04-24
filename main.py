9de0a33a09769ae2ede0e236d88c5096da3b6aa2
KevinStiven04 <kevinmontoya0614@gmail.com>
2026-04-23 22:43:44 -0500

    Configuaracion Main

--- main.py
+++ main.py
@@ -8,4 +8,20 @@ Documentación interactiva: http://127.0.0.1:8000/docs
 Para crear tablas en la base de datos, usa: python init_db.py
 """
 
-from api.app import app
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "https://sistema-reservas-flax.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # o ["*"] para probar
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
